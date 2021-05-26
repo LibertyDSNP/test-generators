43ffb4d5a1b6f7b2efe8cba82d8920aee1d0065d
shannonwells <shannonwells@users.noreply.github.com>
2021-05-25 17:16:26 -0700

    udpate our exports so package users can get to all of them

--- src/test/addresses.test.ts
+++ src/test/addresses.test.ts
@@ -1,38 +1,34 @@
-import {
-  generateEthereumAddress,
-  getNPrefabAddresses,
-  getPrefabAddress,
-} from "../addresses";
+import { addresses } from "..";
 
 describe("addresses", () => {
   it("generateEthereumAddress works", () => {
-    const addr = generateEthereumAddress();
+    const addr = addresses.generateEthereumAddress();
     expect(addr.length).toEqual(22);
     expect(addr).toMatch(/^0x[0-9a-f]+/);
   });
 
   const zeroIndexRegex = /00000$/;
   it("getPrefabAddress works", () => {
-    const res = getPrefabAddress(0);
+    const res = addresses.getPrefabAddress(0);
     expect(res).toHaveLength(42);
     expect(res).toMatch(zeroIndexRegex);
-    expect(getPrefabAddress(999)).toMatch(/00999$/);
+    expect(addresses.getPrefabAddress(999)).toMatch(/00999$/);
 
-    expect(() => getPrefabAddress(3838383)).toThrowError(
+    expect(() => addresses.getPrefabAddress(3838383)).toThrowError(
       "3838383 exceeds the maximum 9999"
     );
   });
 
   it("getNPrefabAddresses works", () => {
-    expect(getNPrefabAddresses(1)[0]).toMatch(zeroIndexRegex);
+    expect(addresses.getNPrefabAddresses(1)[0]).toMatch(zeroIndexRegex);
 
-    let manyAddrs = getNPrefabAddresses(5);
+    let manyAddrs = addresses.getNPrefabAddresses(5);
     expect(manyAddrs).toHaveLength(5);
 
-    manyAddrs = getNPrefabAddresses(33);
+    manyAddrs = addresses.getNPrefabAddresses(33);
     expect(manyAddrs).toHaveLength(33);
 
-    expect(() => getNPrefabAddresses(3838383)).toThrowError(
+    expect(() => addresses.getNPrefabAddresses(3838383)).toThrowError(
       "3838383 exceeds the maximum 10,000"
     );
   });
